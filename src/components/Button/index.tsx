6f0458c987d0853131c490f2a3230e4605583784
Pedro Barbosa <p.brbsa@gmail.com>
2024-02-23 23:27:39 -0300

    fix: formatting

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -11,13 +11,13 @@ export const Button = ({
 }: ButtonProps) => {
   return (
     <button
+      type={type}
+      disabled={isDisabled}
+      onClick={onClick}
       className="max-w-xl w-5/12 min-w-[21rem]
                 rounded-xl disabled:opacity-50 p-4
                 bg-cyan-500 hover:brightness-90
                 text-center text-lg font-bold text-white"
-      type={type}
-      disabled={isDisabled}
-      onClick={onClick}
     >
       {isLoading ? 'Carregando...' : label}
     </button>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,14 +13,14 @@ export const Input = ({ registerFunction, inputName }: InputProps) => {
       <label htmlFor={inputName} className="text-left text-sm w-full">Digite o CEP</label>
       <input
         id={inputName}
+        type="number"
+        placeholder="00000000"
         style={{ backgroundImage: `url(${SearchIcon.src})`, backgroundSize: '12% 30%' }}
         className="dark:bg-grey-800 min-w-[21rem] py-5 pl-9 pr-4 rounded-xl
                   placeholder:text-grey-300 dark:placeholder:text-grey-500 placeholder:text-base
                   focus:outline focus:outline-2 focus:outline-cyan-500
                   bg-no-repeat bg-left
                   "
-        type="number"
-        placeholder="00000000"
         {...registerFunction}
       />
     </div>
